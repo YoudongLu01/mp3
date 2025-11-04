8d3834b228ca94c5930dd0a9550f98f57d5ca5d1
Youdong Lu <youdong@Youdongs-MacBook-Air.local>
2025-11-03 22:31:58 -0600

    YDL MP3

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 // Connect to a MongoDB --> Uncomment this once you have a connection string!!
-mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //add listener to test if connection successfully
 mongoose.connection.on('connected', () => {
